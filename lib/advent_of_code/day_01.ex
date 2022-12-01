fe9db6e60b234aff9f6e91272f7bade8869b4446
BIRSAx2 <mouhieddinesabir@gmail.com>
2022-12-01 14:10:18 +0000

    Added solutions to day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,38 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  defp parse(args) do
+    args
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn calories ->
+      calories
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
   end
 
-  def part2(_args) do
+  defp sum_each_elve_calories(calories) do
+    calories
+    |> Enum.map(&Enum.sum/1)
+  end
+
+  defp take_top_3(calories) do
+    calories
+    |> Enum.sort(&(&1 > &2))
+    |> Enum.take(3)
+  end
+
+  def part1(args) do
+    input = parse(args)
+
+    input
+    |> sum_each_elve_calories()
+    |> Enum.max()
+  end
+
+  def part2(args) do
+    args
+    |> parse()
+    |> sum_each_elve_calories()
+    |> take_top_3()
+    |> Enum.sum()
   end
 end
