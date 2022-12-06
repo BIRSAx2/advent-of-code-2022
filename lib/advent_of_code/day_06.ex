f29777b37710f45f89ebce9e95b7be63f8e71c49
BIRSAx2 <mouhieddinesabir@gmail.com>
2022-12-06 16:00:43 +0000

    Added solutions to day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,29 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  defp parse(args) do
+    args
+    |> String.trim()
   end
 
-  def part2(_args) do
+  defp find_start_of_packet(signal, size \\ 4) do
+    signal
+    |> String.graphemes()
+    |> Enum.chunk_every(size, 1, :discard)
+    |> Enum.take_while(fn chunk ->
+      Enum.uniq(chunk) != chunk
+    end)
+    |> length()
+    |> then(&(&1 + size))
+  end
+
+  def part1(args) do
+    args
+    |> parse
+    |> find_start_of_packet()
+  end
+
+  def part2(args) do
+    args
+    |> parse
+    |> find_start_of_packet(14)
   end
 end
